2aad2246c5b7fc2f98a24c93578ffffb62f58748
ITZ-PRIYANSHU-RAJ <priyanshue722@gmail.com>
2026-08-05 12:18:26 +0530

    login logic

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -63,3 +63,54 @@ export const registerUser = async(req,res)=>{
 
 
 
+export const loginUser = async(req,res) =>{
+    try {
+        const{email,password}=req.body;
+
+        if(!email || !password){
+            return res.status(400).json({
+                success:false,
+                message:"Email and password are required",
+            });
+        }
+
+        const user = await User.findOne({email});
+
+        if(!user){
+            return res.status(400).json({
+                success: false,
+                message: "Invalid email",
+            });
+        }
+
+        const isPasswordCorrect = await bcrypt.comapre(
+            password,
+            user.password
+        );
+        if(!isPasswordCorrect){
+            return res.status(400).json({
+                success:false,
+                message:"Incorrect password",
+            });
+        }
+
+        generateToken(user._id,res);
+
+        res.status(200).json({
+            success:true,
+            message:"Login successful",
+            user:{
+                _id:user._id,
+                fullname:user.fullname,
+                email:user.email,
+            }
+        });
+    } catch (error) {
+        console.error("Login Error:",error.message);
+
+        res.status(500).json({
+            success:false,
+            message:"Internal Server Error",
+        });
+    }
+};
