eff06318802ec1764e9ad06b75bdad06c3bf3512
ITZ-PRIYANSHU-RAJ <priyanshue722@gmail.com>
2026-08-10 12:36:16 +0530

    update profile controller

--- src/controllers/profileController.js
+++ src/controllers/profileController.js
@@ -43,3 +43,29 @@ export const updateProfile = async (req, res) => {
     }
 };
 
+export const getProfile = async (req,res)=>{
+    try{
+        const{userId} = req.params;
+
+        const user = await User.findById(userId).select("-password");
+
+        if(!user){
+            return res.status(404).json({
+                success:false,
+                message: "User not found",
+            });
+        }
+
+        res.status(200).json({
+            success:true,
+            user,
+        });
+    }catch(error){
+        console.error("Get Profile Error:",error.message);
+
+        res.status(500).json({
+            success:false,
+            message:"Internal Server Error",
+        });
+    }
+};
