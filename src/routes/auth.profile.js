7a3312ed77663735b5a14d0f44de2e4d868e2f97
ITZ-PRIYANSHU-RAJ <priyanshue722@gmail.com>
2026-08-10 12:45:39 +0530

    profile authentication

---
+++ src/routes/auth.profile.js
@@ -0,0 +1,14 @@
+import express from "express";
+import{
+    updateProfile,
+    getProfile,
+} from "../controllers/profileController.js";
+import protectRoute from "../middlewares/authMiddleware.js";
+
+const router = express.Router();
+
+router.put("/update",protectRoute,updateProfile);
+
+router.get("/:userId",protectRoute,getProfile);
+
+export default router;
