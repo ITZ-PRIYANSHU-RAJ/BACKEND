39dd1b799dc8f25a075f49cffd2e6bf14e15a09d
ITZ-PRIYANSHU-RAJ <priyanshue722@gmail.com>
2026-08-10 12:28:34 +0530

    update profile

--- src/controllers/profileController.js
+++ src/controllers/profileController.js
@@ -1,51 +1,45 @@
 import User from "../models/User.js"
-export const updateProfile = async(req,res)=>{
-    try{
-        const{fullName, username, bio, profilePic}= req.body; //update profile
 
+export const updateProfile = async (req, res) => {
+    try {
+        const { fullName, username, bio, profilePic } = req.body;
         const userId = req.user._id;
 
         const user = await User.findById(userId);
 
-        if(!user){
+        if (!user) {
             return res.status(400).json({
-                success:false,
-                message:"User not found",
+                success: false,
+                message: "User not found",
             });
         }
-        if(fullName !== undefined){
-            user.fullName = fullName;
-        }
-        if(username !== undefined){
-            user.username = username;
-        }
-        if(bio !== undefined){
-            user.bio = bio;
-        }
-        if(profilePic !== undefined){
-            user.profilePic = profilePic;
 
-            await user.save();
+        if (fullName !== undefined) user.fullName = fullName;
+        if (username !== undefined) user.username = username;
+        if (bio !== undefined) user.bio = bio;
+        if (profilePic !== undefined) user.profilePic = profilePic;
 
-            const updateUser = {
-                _id: user._id,
-                fullName: user.fullName,
-                username: user.username,
-                bio: user.bio,
-                profilePic: user.profilePic,
-            };
-            res.status(200).json({
-             success:true,
-             message:"Profile updated successfully",
-             user:updateUser,
-            });
-        }
-    }catch(error){
-            console.error("Update Profile Error:",error.message);
+        await user.save();
 
-            res.status(500).json({
-                success:false,
-                message:"Internal Server Error",
-            });
-        }
-}
+        const updatedUser = {
+            _id: user._id,
+            fullName: user.fullName,
+            username: user.username,
+            bio: user.bio,
+            profilePic: user.profilePic,
+        };
+
+        res.status(200).json({
+            success: true,
+            message: "Profile updated successfully",
+            user: updatedUser,
+        });
+    } catch (error) {
+        console.error("Update Profile Error:", error.message);
+        res.status(500).json({
+            success: false,
+            message: "Internal Server Error",
+        });
+    }
+};
+
