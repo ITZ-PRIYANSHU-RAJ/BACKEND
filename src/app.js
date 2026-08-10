ede7fe312f345feb21407f2e236e525c5a9c03cc
ITZ-PRIYANSHU-RAJ <priyanshue722@gmail.com>
2026-08-10 12:50:08 +0530

    connecting endpoint

--- src/app.js
+++ src/app.js
@@ -2,12 +2,14 @@ import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import authRoutes from "./routes/auth.Routes.js"
+import authProfile from "./routes/auth.profile.js"
 const app = express();
 
 app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 
-app.use("/api/auth", authRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/profile",authProfile);
 
 export default app;
