9bacc5026645cd1f7782022979d500b7bdc52a1e
ITZ-PRIYANSHU-RAJ <priyanshue722@gmail.com>
2026-08-11 11:40:21 +0530

    search controller

---
+++ src/controllers/userController.js
@@ -0,0 +1,55 @@
+import User from "../models/User";
+
+export const searchUsers = async (req,res) =>{
+    try {
+        const {query} =req.query;
+
+        if(!query || query.trim() === ""){
+            return res.status(400).json({
+                success:false,
+                message:"Search query is required",
+            });
+        }
+
+    const users = await User.find({
+        $or:[
+            {
+                username:{
+                    $regex: query,
+                    $options:"i"
+                },
+            },
+            {
+                fullName:{
+                    $regex: query,
+                    $options:"i"
+                },
+            },
+            {
+                email:{
+                    $regex: query,
+                    $options:"i"
+                },
+            },
+        ],
+
+        _id:{
+            $ne:req.user._id,
+        },
+    })
+      .select("-password")
+      .limit(20);
+
+      res.status(200).json({
+        success:true,
+        users,
+      })
+    } catch (error) {
+        console.error("Search Users Error:",error.message);
+
+        res.status(500).json({
+            success:false,
+            message:"Internal Server Error",
+        });
+    }
+};
