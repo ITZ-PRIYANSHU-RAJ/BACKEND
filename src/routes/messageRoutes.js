e4e4bb3d071df53c32645e2ed8d85ae5a7200604
ITZ-PRIYANSHU-RAJ <priyanshue722@gmail.com>
2026-08-11 21:26:59 +0530

    message routes

---
+++ src/routes/messageRoutes.js
@@ -0,0 +1,10 @@
+import express from "express";
+import {
+    sendMessage,
+    getMessages
+} from "../controllers/messageController.js"
+import protectRoute from "../middlewares/authMiddleware.js";
+
+const router = express.Router();
+router.post("/send",protectRoute,sendMessage);
+router.get("/:userId",protectRoute,getMessages);
