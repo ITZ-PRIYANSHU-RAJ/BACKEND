29b3515a0ac273a2a4bdbe215826f390d6bbd23e
ITZ-PRIYANSHU-RAJ <priyanshue722@gmail.com>
2026-08-12 13:31:58 +0530

    socket io

---
+++ src/lib/socket.js
@@ -0,0 +1,25 @@
+const onlineUsers = new Map();
+
+export const initializeSocket = (io) =>{
+    io.on("connection",(socket)=>{
+        console.log("User connected:",socket.id);
+
+        socket.on("user-online",(userId)=>{
+            onlineUsers.set(userId.toString(),socket.id);
+
+            console.log("Online users:",onlineUsers);
+        });
+
+        socket.on("disconnect",()=>{
+            for(const[userId,socketId]of onlineUsers.entries()){
+                if(socketId === socket.id){
+                    onlineUsers.delete(userId);
+                    console.log("User offline:",userId);
+                    break;
+                }
+            }
+
+            console.log("User disconnected:",socket.id)
+        })
+    })
+}
