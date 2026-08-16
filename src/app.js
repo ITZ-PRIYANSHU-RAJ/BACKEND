f4dfd86df7fe27e3cf9df36c745f028fba0ea239
ITZ-PRIYANSHU-RAJ <priyanshue722@gmail.com>
2026-08-16 11:54:06 +0530

    bug fixed

--- src/app.js
+++ src/app.js
@@ -8,7 +8,12 @@ import messageRoutes from "./routes/messageRoutes.js";
 import cors from "cors";
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
