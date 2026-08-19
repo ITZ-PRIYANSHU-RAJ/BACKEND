001c6608497ac170fa9acbe7ff2b8132c9c289f5
ITZ-PRIYANSHU-RAJ <priyanshue722@gmail.com>
2026-08-19 12:17:51 +0530

    deployement

--- src/server.js
+++ src/server.js
@@ -14,10 +14,14 @@ const PORT = process.env.PORT || 5000;
 const server = http.createServer(app);
 
 const io = new Server(server, {
-    cors: {
-        origin: "http://localhost:5173",
-        credentials: true,
-    },
+  cors: {
+    origin: [
+      "http://localhost:5173",
+      process.env.CLIENT_URL,
+    ],
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
 });
 
 initializeSocket(io);
